a03c16571fdc9ee45ecb5b1d2be8e12399f5a35d
Elliott Lewandowski <elewandowski30@gmail.com>
2024-09-13 13:19:04 -0800

    Basic setup for homework 2

---
+++ homework1/msarray.hpp
@@ -0,0 +1,12 @@
+
+
+#ifndef FILE_MSARRY_HPP_INCLUDED
+#define FILE_MSARRY_HPP_INCLUDED
+
+template <typename ValType>
+class MSArray 
+{
+    public:
+        using value_type = ValType;
+};
+#endif // FILE_MSARRY_HPP_INCLUDED
