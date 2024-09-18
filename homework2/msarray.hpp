fb89c27ea82f8000f276e96ad014e2a1b8782880
Elliott Lewandowski <elewandowski30@gmail.com>
2024-09-18 10:18:30 -0800

    Set up basic functions, still not compiling, renamed foldres to match assignment numbers

---
+++ homework2/msarray.hpp
@@ -0,0 +1,132 @@
+
+
+#ifndef FILE_MSARRY_HPP_INCLUDED
+#define FILE_MSARRY_HPP_INCLUDED
+
+#include <cstddef>
+
+template <typename ValType>
+class MSArray 
+{ 
+    public:
+        using value_type = ValType;
+        using size_type = size_t;
+
+        // Default ctor
+        MSArray() 
+        {
+            
+        }
+
+        // 1-param ctor
+        MSArray(int num_elements) 
+        {
+
+        }
+
+        // 2-param ctor
+        MSArray(int num_elements, ValType value) 
+        {
+
+        }
+
+        // dtor
+        ~MSArray() 
+        {
+            delete m_array;
+        }
+
+        // Copy ctor
+        MSArray(const MSArray<ValType> & other) 
+        {
+
+        }
+
+        // Copy Assignment
+        MSArray & operator=(const MSArray<ValType> & rhs) 
+        {
+
+        }
+
+        // Move ctor
+        MSArray(MSArray && other) 
+        {
+
+        }
+
+        // Move Assignment
+        MSArray<ValType> & operator=(MSArray<ValType> && other) 
+        {
+
+        }
+
+        ValType & operator [](size_type index)
+        {
+
+        }
+
+        const ValType & operator [](size_type index) const
+        {
+
+        }
+
+        size_type size()
+        {
+            return m_size;
+        }
+
+        ValType * begin()
+        {
+
+        }
+
+        const ValType * begin() const
+        {
+
+        }
+
+        ValType * end()
+        {
+
+        }
+
+        const ValType * end() const
+        {
+
+        }
+
+        bool operator==(const MSArray<ValType> & rhs)
+        {
+
+        }
+
+        bool operator!=(const MSArray<ValType> & rhs)
+        {
+
+        }
+
+        bool operator<(const MSArray<ValType> & rhs)
+        {
+
+        }
+
+        bool operator<=(const MSArray<ValType> & rhs)
+        {
+
+        }
+
+        bool operator>(const MSArray<ValType> & rhs)
+        {
+
+        }
+
+        bool operator>=(const MSArray<ValType> & rhs)
+        {
+
+        }
+
+    private:
+        size_t m_size;
+        ValType *  m_array;
+};
+#endif // FILE_MSARRY_HPP_INCLUDED
