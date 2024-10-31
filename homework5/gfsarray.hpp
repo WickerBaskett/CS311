aef0fdd981a7bc33c2e22cd282d058caa9b06f82
Elliott Lewandowski <erlewandowski@alaska.edu>
2024-10-30 22:37:59 -0800

    Working, just finished up with coding standards

--- homework5/gfsarray.hpp
+++ homework5/gfsarray.hpp
@@ -1,44 +1,14 @@
-// GFSArray.hpp  HAND-OFF
-// VERSION 6
-// Glenn G. Chappell
+// GFSArray.hpp  
+// Elliott R. Lewandowski
 // Started: 2024-10-15
-// Updated: 2024-10-23
+// Last Updated: 2024-10-30
 //
 // For CS 311 Fall 2024
 // Header for class GFSArray
-// Frightfully smart array of int
-// Preliminary to Assignment 5
+// Generic frightfully smart array 
+// for CS311 Assignment 5
 
-// History:
-// - v1:
-//   - Bare-bones only, does not compile. Header & source files,
-//     #ifndef, #include, empty class definition.
-// - v2:
-//   - Add member types value_type, size_type, iterator, const_iterator.
-//   - Add dummy versions (at least) of all member functions, including
-//     dummy return statements for non-void functions. Package compiles.
-//   - Add data members.
-//   - Add class invariants.
-//   - Write (untested versions of) the following member functions:
-//     default ctor, ctor from size (these two are single func), dctor,
-//     op[], size, empty, begin, end, push_back, pop_back.
-// - v3:
-//   - Document exception-safety guarantees for most functions.
-//   - Write copy ctor.
-// - v4:
-//   - Revise class invariants to allow for _data being nullptr if _size
-//     is zero.
-//   - Revise ctor from size, copy ctor accordingly.
-//   - Write move ctor.
-//   - Mark various functions as noexcept.
-// - v5:
-//   - Move func defs to source file: copy & move ops, resize, insert,
-//     erase, swap.
-// - v6:
-//   - Add _capacity data member.
-//   - Revise class invariants & ctors accordingly.
-//   - Add constant DEFAULT_CAP and use it in setting the capacity in
-//     default ctor/ctor from size.
+// Acknowledgements: Glenn G. Chappell wrote the skeleton for this class
 
 #ifndef FILE_GFSArray_HPP_INCLUDED
 #define FILE_GFSArray_HPP_INCLUDED
@@ -47,7 +17,6 @@
 // For std::size_t
 #include <algorithm>
 // For std::max
-
 
 // *********************************************************************
 // class GFSArray - Class definition
@@ -88,8 +57,13 @@ private:
 public:
 
     // Default ctor & ctor from size
+    // Pre:
+    //      thesize >= 0
     // Strong Guarantee
-    explicit GFSArray(size_type thesize=0)
+    // Exception-Neutral
+    // Exceptions:
+    //      std::bad_alloc, any exceptions thrown by value_type member functions
+    explicit GFSArray(size_type thesize = 0)
         :_capacity(std::max(thesize, size_type(DEFAULT_CAP))),
             // _capacity must be declared before _data
          _size(thesize),
@@ -99,22 +73,66 @@ public:
 
     // Copy ctor
     // Strong Guarantee
-    GFSArray(const GFSArray & other);
+    // Exception-Neutral
+    // Exceptions:
+    //      std::bad_alloc, any exceptions thrown by value_type member functions
+    GFSArray(const GFSArray & other)
+        :_capacity(other._capacity),
+        _size(other.size())
+    {
+        try {
+            _data = (other._capacity == 0) ? nullptr
+                                    : new value_type[other._capacity];
+            std::copy(other.begin(), other.end(), begin());
+        }
+        catch (...)
+        {
+            delete [] _data;
+            throw;
+        }
+    }
 
     // Move ctor
     // No-Throw Guarantee
-    GFSArray(GFSArray && other) noexcept;
+    // Exception-Neutral
+    // Does not throw
+    GFSArray(GFSArray && other) noexcept
+        :_capacity(other._capacity),
+         _size(other._size),
+         _data(other._data)
+    {
+        other._capacity = 0;
+        other._size = 0;
+        other._data = nullptr;
+    }
 
     // Copy assignment operator
-    // ??? Guarantee
-    GFSArray<ValType> & operator=(const GFSArray<ValType> & other);
+    // Strong Guarantee
+    // Exception-Neutral
+     // Exceptions:
+    //      any exceptions thrown by value_type copy constructor
+    GFSArray<ValType> & operator=(const GFSArray<ValType> & other)
+    {
+        auto other_copy = other;
+        swap(other_copy);
+        return *this;
+    }
+
 
     // Move assignment operator
     // No-Throw Guarantee
-    GFSArray & operator=(GFSArray && other) noexcept;
+    // Exception-Neutral
+    // Does not throw
+    GFSArray & operator=(GFSArray && other) noexcept
+    {
+        swap(other);
+        return *this;
+    }
 
     // Dctor
     // No-Throw Guarantee
+    // Exception-Neutral
+    // Does not throw
     ~GFSArray()
     {
         delete [] _data;
@@ -125,8 +143,10 @@ public:
 
     // operator[] - non-const & const
     // Pre:
-    //     ???
+    //     index is in the range [0, _size - 1]
     // No-Throw Guarantee
+    // Exception-Neutral
+    // Does not throw
     value_type & operator[](size_type index)
     {
         return _data[index];
@@ -141,6 +161,8 @@ public:
 
     // size
     // No-Throw Guarantee
+    // Exception-Neutral
+    // Does not throw
     size_type size() const noexcept
     {
         return _size;
@@ -148,6 +170,8 @@ public:
 
     // empty
     // No-Throw Guarantee
+    // Exception-Neutral
+    // Does not throw
     bool empty() const noexcept
     {
         return size() == 0;
@@ -155,6 +179,8 @@ public:
 
     // begin - non-const & const
     // No-Throw Guarantee
+    // Exception-Neutral
+    // Does not throw
     iterator begin() noexcept
     {
         return _data;
@@ -166,6 +192,8 @@ public:
 
     // end - non-const & const
     // No-Throw Guarantee
+    // Exception-Neutral
+    // Does not throw
     iterator end() noexcept
     {
         return begin() + size();
@@ -176,39 +204,92 @@ public:
     }
 
     // resize
-    // ??? Guarantee
-    void resize(size_type newsize);
+    // Pre:
+    //      newSize >= 0
+    // Strong Guarantee
+    // Exception-Neutral
+    void resize(size_type newSize)
+    {
+        if (newSize <= _capacity)
+        {
+            _size = newSize;
+        }
+        else
+        {
+            size_type newCapacity = (newSize > (_capacity * 2)) ? newSize
+                    : (_capacity * 2);
+
+            value_type * newData;
+
+            try {
+                newData = new value_type[newCapacity];
+                std::copy(begin(), end(), newData);
+            } 
+            catch (...)
+            {
+                delete [] newData;
+                throw;
+            }
+
+            delete [] _data;
+            _data = newData;
+            _capacity = newCapacity;
+            _size = newSize;
+        }
+    }
 
     // insert
     // Pre:
-    //     ???
-    // ??? Guarantee
+    //     pos must be a valid iterator for an element of _data
+    // Basic Guarantee
+    // Exception-Neutral
     iterator insert(iterator pos,
-                    value_type item);
-        // Above, passing by value is appropriate, since our value type
-        // is int. However, if the value type is changed, then a
-        // different parameter-passing method may need to be used.
+                    const value_type & item)
+    {
+        // Save index of pos
+        size_type index = std::distance(begin(), pos);
+
+        resize(_size + 1);
+
+        // Resize might reallocate and copy so we make a new iterator 
+        //   at the same index just in case
+        iterator new_pos = std::next(begin(), index);
+        std::rotate(new_pos, std::next(end(), -1), end());
+
+        *new_pos = item;
+        return new_pos;
+    }
 
     // erase
     // Pre:
-    //     ???
-    // ??? Guarantee
-    iterator erase(iterator pos);
+    //     pos must be a valid iterator for an element of _data
+    // Basic Guarantee
+    // Exception-Neutral
+    iterator erase(iterator pos)
+    {
+        // Follows std::vectors example, returns last if the array is empty
+        if (_size == 0)
+        {
+            return end();
+        }
+        std::rotate(pos, std::next(pos, 1), end());
+        resize(_size - 1);
+        return pos;
+    }
 
     // push_back
-    // ??? Guarantee
-    void push_back(value_type item)
-        // Above, passing by value is appropriate, since our value type
-        // is int. However, if the value type is changed, then a
-        // different parameter-passing method may need to be used.
+    // Basic Guarantee
+    // Exception-Neutral
+    void push_back(const value_type & item)
     {
         insert(end(), item);
     }
 
     // pop_back
     // Pre:
-    //     ???
-    // ??? Guarantee
+    //     _size must be at least 1
+    // Basic Guarantee
+    // Exception-Neutral
     void pop_back()
     {
         erase(end()-1);
@@ -216,7 +297,14 @@ public:
 
     // swap
     // No-Throw Guarantee
-    void swap(GFSArray & other) noexcept;
+    // Exception-Neutral
+    // Does not throw
+    void swap(GFSArray & other) noexcept
+    {
+        std::swap(_capacity, other._capacity);
+        std::swap(_size, other._size);
+        std::swap(_data, other._data);
+    }
 
 // ***** GFSArray: data members *****
 private:
